e55a70d883b95035e6fd41ed13ec29a8e75a7aa9
jparedes <jparedes@localhost>
2025-11-14 06:10:11 -0500

    sout

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -16,6 +16,7 @@ public class GUI {
     public static void main(String[] args) {
         // TODO code application logic here
         new Menu().setVisible(true);
+        System.out.println("test");
     }
     
 }
